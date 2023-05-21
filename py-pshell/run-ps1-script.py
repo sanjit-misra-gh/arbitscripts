453c58dbc896872e690c2ccd40a7e183ff1ff98f
sanjit-misra-gh <sanjitmisra@hotmail.com>
2023-05-21 18:40:21 +0530

    refactored to accept command line args in the ps script

--- py-pshell/run-ps1-script.py
+++ py-pshell/run-ps1-script.py
@@ -1,21 +1,21 @@
 import subprocess, sys, time
 
-def execute_ps1(scriptpath):
+def execute_ps1(scriptpath, args):
+
     tstart = time.perf_counter()
-    p = subprocess.Popen(["powershell.exe", 
-              scriptpath], 
+    pshell_process = subprocess.Popen(["powershell.exe", 
+              scriptpath, args], 
               stdout=sys.stdout)
-    p.communicate()
+    output, errors = pshell_process.communicate()
     tend = time.perf_counter()
+
+    # Need to play around with the returncode value to handle errors. Also, wrap this in a try...except 
+    print("Process Response : " + str(pshell_process.returncode))
+    
     print(f"Conversion time is {tend - tstart:0.4f} seconds")
 
 
 if __name__ == '__main__':
-    process_response = execute_ps1("D:\\work\\py-pshell\\xls-csv-converter.ps1")
+    process_response = execute_ps1("D:\\work\\arbitscripts\\py-pshell\\xls-csv-converter.ps1", " -source C:\\Users\\sanji\\Downloads\\small-test-file.xlsx -dest C:\\Users\\sanji\\Downloads\\converted-files")
     
     print(process_response)
-
-    # if process_response.returncode != 0:
-    #     print("An error occured: %s", process_response.stderr)
-    # else:
-    #     print("Hello command executed successfully!")
--- py-pshell/xls-csv-converter.ps1
+++ py-pshell/xls-csv-converter.ps1
@@ -1,8 +1,13 @@
+param(
+        [String]$source,
+        [String]$dest
+    )
+
 $Excel = New-Object -ComObject Excel.Application
-$wb = $Excel.Workbooks.Open('C:\Users\sanji\Downloads\testxls.xls')
+$wb = $Excel.Workbooks.Open($source)
 $timestamp = Get-Date -Format "dd/MM/yyyy hhmmss"
 foreach ($ws in $wb.Worksheets) 
     {
-        $ws.SaveAs('C:\Users\sanji\Downloads\' + $timestamp + '.csv', 6)
+        $ws.SaveAs( $dest + "\" + $timestamp + '.csv', 6)
     }
 $Excel.Quit()
