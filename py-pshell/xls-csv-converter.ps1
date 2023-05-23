2876f554b86be46ff49e7d79d812a5512d72cb6e
sanjit-misra-gh <sanjitmisra@hotmail.com>
2023-05-23 10:48:12 +0530

    formatting mods

--- py-pshell/xls-csv-converter.ps1
+++ py-pshell/xls-csv-converter.ps1
@@ -3,9 +3,7 @@ param(
         [String]$dest
     )
 
-try{
-    $Excel = New-Object -ComObject Excel.Application
-}
+try { $Excel = New-Object -ComObject Excel.Application }
 catch{
     "FATAL | Cannot instantiate Excel" | out-File errors.txt -Append
     throw "Error instantiating Excel object"
@@ -13,31 +11,16 @@ catch{
 
 $timestamp = Get-Date -Format "dd/MM/yyyy hhmmss"
 
-try{
-    $wb = $Excel.Workbooks.Open($source)
-}
-catch{ 
-    "Invalid source path | " + $source | out-File errors.txt -Append
-}
-finally{
-    $Excel.Quit()
-}
+try { $wb = $Excel.Workbooks.Open($source) }
+catch { "Invalid source path | " + $source | out-File errors.txt -Append }
+finally { $Excel.Quit() }
 
 try{
-    if (-not (Test-Path $dest)) {
-        
-        throw [System.IO.FileNotFoundException]
-    }
-     
+    if (-not (Test-Path $dest)) { throw [System.IO.FileNotFoundException] }
     foreach ($ws in $wb.Worksheets) 
         {
             $ws.SaveAs( $dest + "\" + $timestamp + '.csv', 6)
         }
     }   
-catch{
-        "Invalid destination path | " + $dest | out-File errors.txt -Append
-}
-finally{
-    Write-Host "Closing the Excel instance"
-    $Excel.Quit()
-}
+catch { "Invalid destination path | " + $dest | out-File errors.txt -Append }
+finally { $Excel.Quit() }
